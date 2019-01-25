34ce9bee65f2f3998ed3ac2863e6183b9f9f8047
Clément <clement@macbook-pro-de-clement.home>
2019-01-25 19:44:28 +0100

    Test Component example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import Header from './Components/Header/index';
+import Test from './Components/Text/index';
 
 class App extends Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <Test 
+          phrase="J'ai envie de manger un macdo sur la plage
+                  Demain matin, je passerai l'aspirateur puis j'aroserai mes plantes.
+                  Ensuite, je préparerai mes affaires pour aller chez Maëlle"
+        />
+      </div>
     );
   }
 }
--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import './header.css';
 
